35f217f4a324ff7cc3aded72480dfe695c637e7a
Fashakin Olashubomi <subomifasakin@outlook.com>
2026-02-09 10:32:51 +0100

    updated swagger docs

--- src/modules/users/entities/user.dto.ts
+++ src/modules/users/entities/user.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsUrl,
   IsDate,
@@ -9,21 +10,36 @@ import {
 } from 'class-validator';
 
 export class CachedUserInfo {
+  @ApiProperty({
+    description: 'The name of the user',
+    example: 'John Doe',
+    minLength: 3,
+    maxLength: 30,
+  })
   @IsString({ message: 'Invalid name' })
   @MinLength(3, { message: 'Name is too short' })
   @MaxLength(30, { message: 'Name is too long' })
   name: string;
 
+  @ApiProperty({
+    example: 'example@gmail.com',
+    description: 'The email of the user',
+  })
   @IsEmail({}, { message: 'Invalid email' })
   email: string;
 
+  @ApiProperty({ description: 'The date the users account was created' })
   @IsDate({ message: 'Invalid date' })
   created_at: Date;
 
+  @ApiProperty({
+    description: 'The picture of the user gotten from google, if any',
+  })
   @IsUrl({}, { message: 'Invalid picture url' })
   @IsOptional()
   picture: string | null;
 
+  @ApiProperty({ description: 'The date the users account was last updated' })
   @IsDate({ message: 'Invalid date' })
   updated_at: Date;
 }
--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,7 +1,7 @@
 import { type Response, type Request } from 'express';
 
 import { ConfigService } from '@nestjs/config';
-import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { ApiBody, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import {
   Body,
   Controller,
@@ -30,31 +30,32 @@ export class UsersController {
     private readonly configService: ConfigService,
   ) {}
 
-  @Get('me')
   @ApiOperation({ summary: 'Get logged in users info' })
   @ApiResponse({ status: 200, description: 'Success', type: CachedUserInfo })
   @ApiResponse({ status: 404, description: 'User does not exist' })
+  @Get('me')
   async getMyInfo(@Req() req: Request) {
     return this.UsersService.getMyInfo(req.user.id);
   }
 
-  @Patch('me')
   @HttpCode(200)
   @ApiOperation({ summary: 'Update logged in users info' })
   @ApiResponse({ status: 200, description: 'Success', type: CachedUserInfo })
   @ApiResponse({ status: 404, description: 'User does not exist' })
+  @ApiBody({ type: UpdateUserDto })
+  @Patch('me')
   async updateMyInfo(@Req() req: Request, @Body() dto: UpdateUserDto) {
     return this.UsersService.updateMyInfo(req.user.id, dto);
   }
 
-  @Delete('me')
-  @HttpCode(200)
   @ApiResponse({
     status: 200,
     description: 'Users account deleted successfully',
   })
   @ApiResponse({ status: 404, description: 'User was not found' })
   @ApiOperation({ summary: 'Deletes logged in users account permanently' })
+  @HttpCode(200)
+  @Delete('me')
   async deleteMyInfo(
     @Req() req: Request,
     @Res({ passthrough: true }) res: Response,
