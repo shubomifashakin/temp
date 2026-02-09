6cdcf85a4f93a56877e7f8059ae57fc374341e1b
Fashakin Olashubomi <subomifasakin@outlook.com>
2026-02-09 10:15:26 +0100

    added users controller and service tests

--- src/modules/users/users.controller.spec.ts
+++ src/modules/users/users.controller.spec.ts
@@ -1,5 +1,50 @@
+import { type Request, type Response } from 'express';
+
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule, JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
+
+import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+
+import { RedisModule } from '../../core/redis/redis.module';
+import { RedisService } from '../../core/redis/redis.service';
+
+import { DatabaseModule } from '../../core/database/database.module';
+import { DatabaseService } from '../../core/database/database.service';
+
+const mockDatabaseService = {
+  user: {
+    findUniqueOrThrow: jest.fn(),
+    delete: jest.fn(),
+    update: jest.fn(),
+  },
+};
+
+const mockRedisService = {
+  get: jest.fn(),
+  set: jest.fn(),
+  delete: jest.fn(),
+};
+
+const mockJwtService = {
+  verifyAsync: jest.fn(),
+};
+
+const testUserId = 'test-user-id';
+const mockRequest = {
+  user: {
+    id: testUserId,
+  },
+} as jest.Mocked<Request>;
+
+const mockResponse = {
+  clearCookie: jest.fn(),
+} as unknown as jest.Mocked<Response>;
+
+const mockConfigService = {
+  getOrThrow: jest.fn(),
+};
 
 describe('UsersController', () => {
   let controller: UsersController;
@@ -7,12 +52,108 @@ describe('UsersController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-    }).compile();
+      providers: [UsersService],
+      imports: [DatabaseModule, RedisModule, JwtModule, ConfigModule],
+    })
+      .overrideProvider(DatabaseService)
+      .useValue(mockDatabaseService)
+      .overrideProvider(RedisService)
+      .useValue(mockRedisService)
+      .overrideProvider(JwtService)
+      .useValue(mockJwtService)
+      .overrideProvider(ConfigService)
+      .useValue(mockConfigService)
+      .compile();
 
     controller = module.get<UsersController>(UsersController);
+    jest.clearAllMocks();
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should get the users info from database', async () => {
+    const foundUser = {
+      name: 'Test User',
+      email: 'test@email.com',
+      picture: null,
+      updated_at: new Date(Date.now() * 1000),
+      created_at: new Date(Date.now() * 10),
+    };
+
+    mockRedisService.get.mockResolvedValue({ success: true, data: null });
+
+    mockDatabaseService.user.findUniqueOrThrow.mockResolvedValue(foundUser);
+
+    const res = await controller.getMyInfo(mockRequest);
+
+    expect(res).toEqual(foundUser);
+  });
+
+  it('should get the users info from redis', async () => {
+    const foundUser = {
+      name: 'Test User',
+      email: 'test@email.com',
+      picture: null,
+      updated_at: new Date(Date.now() * 1000),
+      created_at: new Date(Date.now() * 10),
+    };
+
+    mockRedisService.get.mockResolvedValue({ success: true, data: foundUser });
+
+    const res = await controller.getMyInfo(mockRequest);
+
+    expect(res).toEqual(foundUser);
+
+    expect(mockDatabaseService.user.findUniqueOrThrow).not.toHaveBeenCalled();
+  });
+
+  it('should update the users account', async () => {
+    const updatedName = 'Updated Name';
+    const updatedUser = {
+      name: updatedName,
+      email: 'test@email.com',
+      picture: null,
+      updated_at: new Date(Date.now() * 1000),
+      created_at: new Date(Date.now() * 10),
+    };
+
+    mockDatabaseService.user.update.mockResolvedValue(updatedUser);
+
+    mockRedisService.set.mockResolvedValue({ success: true });
+
+    const res = await controller.updateMyInfo(mockRequest, {
+      name: updatedName,
+    });
+
+    expect(mockDatabaseService.user.update).toHaveBeenCalledWith({
+      where: {
+        id: testUserId,
+      },
+      data: {
+        name: updatedName,
+      },
+      select: {
+        name: true,
+        email: true,
+        picture: true,
+        updated_at: true,
+        created_at: true,
+      },
+    });
+    expect(res).toEqual(updatedUser);
+  });
+
+  it('should delete the users account', async () => {
+    mockDatabaseService.user.delete.mockResolvedValue(true);
+    mockRedisService.delete.mockResolvedValue({ success: true });
+
+    const res = await controller.deleteMyInfo(mockRequest, mockResponse);
+
+    expect(res.message).toBeDefined();
+
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    expect(mockResponse.clearCookie).toHaveBeenCalledTimes(2);
   });
 });
--- src/modules/users/users.service.spec.ts
+++ src/modules/users/users.service.spec.ts
@@ -1,5 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
+
 import { UsersService } from './users.service';
+
+import { RedisModule } from '../../core/redis/redis.module';
+import { RedisService } from '../../core/redis/redis.service';
+import { DatabaseService } from '../../core/database/database.service';
+import { DatabaseModule } from '../../core/database/database.module';
+
+const mockDatabaseService = {
+  user: {
+    findUniqueOrThrow: jest.fn(),
+    delete: jest.fn(),
+    update: jest.fn(),
+  },
+};
+
+const mockRedisService = {
+  get: jest.fn(),
+  set: jest.fn(),
+  delete: jest.fn(),
+};
 
 describe('UsersService', () => {
   let service: UsersService;
@@ -7,12 +27,107 @@ describe('UsersService', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [UsersService],
-    }).compile();
+      imports: [DatabaseModule, RedisModule],
+    })
+      .overrideProvider(DatabaseService)
+      .useValue(mockDatabaseService)
+      .overrideProvider(RedisService)
+      .useValue(mockRedisService)
+      .compile();
 
     service = module.get<UsersService>(UsersService);
+
+    jest.clearAllMocks();
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should get the users info from database', async () => {
+    const testUserId = 'test-user-id';
+    const foundUser = {
+      name: 'Test User',
+      email: 'test@email.com',
+      picture: null,
+      updated_at: new Date(Date.now() * 1000),
+      created_at: new Date(Date.now() * 10),
+    };
+
+    mockRedisService.get.mockResolvedValue({ success: true, data: null });
+
+    mockDatabaseService.user.findUniqueOrThrow.mockResolvedValue(foundUser);
+
+    const res = await service.getMyInfo(testUserId);
+
+    expect(res).toEqual(foundUser);
+  });
+
+  it('should get the users info from redis', async () => {
+    const testUserId = 'test-user-id';
+    const foundUser = {
+      name: 'Test User',
+      email: 'test@email.com',
+      picture: null,
+      updated_at: new Date(Date.now() * 1000),
+      created_at: new Date(Date.now() * 10),
+    };
+
+    mockRedisService.get.mockResolvedValue({ success: true, data: foundUser });
+
+    const res = await service.getMyInfo(testUserId);
+
+    expect(res).toEqual(foundUser);
+
+    expect(mockDatabaseService.user.findUniqueOrThrow).not.toHaveBeenCalled();
+  });
+
+  it('should update the users account', async () => {
+    const testUserId = 'test-user-id';
+
+    const updatedName = 'Updated Name';
+    const updatedUser = {
+      name: updatedName,
+      email: 'test@email.com',
+      picture: null,
+      updated_at: new Date(Date.now() * 1000),
+      created_at: new Date(Date.now() * 10),
+    };
+
+    mockDatabaseService.user.update.mockResolvedValue(updatedUser);
+
+    mockRedisService.set.mockResolvedValue({ success: true });
+
+    const res = await service.updateMyInfo(testUserId, {
+      name: updatedName,
+    });
+
+    expect(mockDatabaseService.user.update).toHaveBeenCalledWith({
+      where: {
+        id: testUserId,
+      },
+      data: {
+        name: updatedName,
+      },
+      select: {
+        name: true,
+        email: true,
+        picture: true,
+        updated_at: true,
+        created_at: true,
+      },
+    });
+    expect(res).toEqual(updatedUser);
+  });
+
+  it('should delete the users account', async () => {
+    const testUserId = 'test-user-id';
+
+    mockDatabaseService.user.delete.mockResolvedValue(true);
+    mockRedisService.delete.mockResolvedValue({ success: true });
+
+    const res = await service.deleteMyInfo(testUserId);
+
+    expect(res.message).toBeDefined();
   });
 });
