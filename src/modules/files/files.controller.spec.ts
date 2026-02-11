a43d18e12628e79aa4e26d67158df85b05784c79
Fashakin Olashubomi <subomifasakin@outlook.com>
2026-02-11 16:27:17 +0100

    setting up tests

--- src/modules/files/files.controller.spec.ts
+++ src/modules/files/files.controller.spec.ts
@@ -1,12 +1,32 @@
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
+
+import { FilesService } from './files.service';
 import { FilesController } from './files.controller';
+
+import { S3Module } from '../../core/s3/s3.module';
+import { SqsModule } from '../../core/sqs/sqs.module';
+import { RedisModule } from '../../core/redis/redis.module';
+import { HasherModule } from '../../core/hasher/hasher.module';
+import { DatabaseModule } from '../../core/database/database.module';
 
 describe('FilesController', () => {
   let controller: FilesController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers: [FilesService],
       controllers: [FilesController],
+      imports: [
+        DatabaseModule,
+        RedisModule,
+        S3Module,
+        SqsModule,
+        HasherModule,
+        ConfigModule.forRoot({ isGlobal: true }),
+        JwtModule,
+      ],
     }).compile();
 
     controller = module.get<FilesController>(FilesController);
--- src/modules/files/files.service.spec.ts
+++ src/modules/files/files.service.spec.ts
@@ -1,5 +1,14 @@
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
+
 import { FilesService } from './files.service';
+
+import { S3Module } from '../../core/s3/s3.module';
+import { SqsModule } from '../../core/sqs/sqs.module';
+import { RedisModule } from '../../core/redis/redis.module';
+import { HasherModule } from '../../core/hasher/hasher.module';
+import { DatabaseModule } from '../../core/database/database.module';
 
 describe('FilesService', () => {
   let service: FilesService;
@@ -7,6 +16,15 @@ describe('FilesService', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [FilesService],
+      imports: [
+        ConfigModule.forRoot({ isGlobal: true }),
+        JwtModule,
+        S3Module,
+        SqsModule,
+        RedisModule,
+        HasherModule,
+        DatabaseModule,
+      ],
     }).compile();
 
     service = module.get<FilesService>(FilesService);
