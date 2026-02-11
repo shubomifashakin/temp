c247c28e0828c771ad2490a9a02c711432daed98
Fashakin Olashubomi <subomifasakin@outlook.com>
2026-02-11 16:50:40 +0100

    tests

--- src/modules/files/files.service.spec.ts
+++ src/modules/files/files.service.spec.ts
@@ -7,8 +7,33 @@ import { FilesService } from './files.service';
 import { S3Module } from '../../core/s3/s3.module';
 import { SqsModule } from '../../core/sqs/sqs.module';
 import { RedisModule } from '../../core/redis/redis.module';
+import { RedisService } from '../../core/redis/redis.service';
 import { HasherModule } from '../../core/hasher/hasher.module';
 import { DatabaseModule } from '../../core/database/database.module';
+import { DatabaseService } from '../../core/database/database.service';
+
+const mockDatabaseService = {
+  files: {
+    create: jest.fn(),
+    findUniqueOrThrow: jest.fn(),
+    update: jest.fn(),
+    delete: jest.fn(),
+    findMany: jest.fn(),
+  },
+  shareLinks: {
+    create: jest.fn(),
+    update: jest.fn(),
+    delete: jest.fn(),
+    findMany: jest.fn(),
+    findUniqueOrThrow: jest.fn(),
+  },
+};
+
+const mockRedisService = {
+  get: jest.fn(),
+  set: jest.fn(),
+  delete: jest.fn(),
+};
 
 describe('FilesService', () => {
   let service: FilesService;
@@ -25,7 +50,12 @@ describe('FilesService', () => {
         HasherModule,
         DatabaseModule,
       ],
-    }).compile();
+    })
+      .overrideProvider(DatabaseService)
+      .useValue(mockDatabaseService)
+      .overrideProvider(RedisService)
+      .useValue(mockRedisService)
+      .compile();
 
     service = module.get<FilesService>(FilesService);
   });
