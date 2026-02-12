bec73d2295c4fe2b469f2d82177058cc53a53e78
Fashakin Olashubomi <subomifasakin@outlook.com>
2026-02-12 23:15:22 +0100

    refactor

--- src/modules/files/common/constants.ts
+++ src/modules/files/common/constants.ts
@@ -1,13 +1,14 @@
 export const MAX_FILE_SIZE_BYTES = 80 * 1024 * 1024;
 
 export const ALLOWED_MIME_TYPES = [
-  // images
+  //images
   'image/png',
   'image/jpeg',
   'image/jpg',
   'image/gif',
   'image/webp',
-  // documents
+
+  //documentss
   'application/pdf',
   'application/msword',
   'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
@@ -16,13 +17,15 @@ export const ALLOWED_MIME_TYPES = [
   'application/vnd.ms-powerpoint',
   'application/vnd.openxmlformats-officedocument.presentationml.presentation',
   'text/plain',
-  // audio
+
+  //audio
   'audio/mpeg',
   'audio/mp3',
   'audio/wav',
   'audio/x-wav',
   'audio/ogg',
-  // video
+
+  //video
   'video/mp4',
   'video/quicktime',
 ];
