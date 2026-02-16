9fd45ecc1b26138d454e7d1f66a0fad46d5abb34
Fashakin Olashubomi <subomifasakin@outlook.com>
2026-02-16 20:45:17 +0100

    health controller

--- test/health.e2e-spec.ts
+++ test/health.e2e-spec.ts
@@ -2,9 +2,9 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { App } from 'supertest/types';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
 
-describe('AppController (e2e)', () => {
+describe('HealthController (e2e)', () => {
   let app: INestApplication<App>;
 
   beforeEach(async () => {
@@ -16,10 +16,16 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('/ (GET)', async () => {
+    const response = await request(app.getHttpServer()).get('/health');
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({
+      status: 'ok',
+      timestamp: expect.any(String),
+    });
   });
 });
