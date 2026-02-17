e004d84269fca178d5376084ad3e33fe1373a83d
Fashakin Olashubomi <subomifasakin@outlook.com>
2026-02-17 17:52:01 +0100

    refactored controller tests

--- src/modules/auth/auth.controller.spec.ts
+++ src/modules/auth/auth.controller.spec.ts
@@ -1,61 +1,17 @@
 import { Request, Response } from 'express';
-import { JwtModule, JwtService } from '@nestjs/jwt';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { Test, TestingModule } from '@nestjs/testing';
 
-import {
-  UnauthorizedException,
-  InternalServerErrorException,
-  Logger,
-} from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { Logger } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { Test, TestingModule } from '@nestjs/testing';
 
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 
 import { RedisModule } from '../../core/redis/redis.module';
 import { DatabaseModule } from '../../core/database/database.module';
-import { RedisService } from '../../core/redis/redis.service';
-import { DatabaseService } from '../../core/database/database.service';
 
 import { TOKEN } from '../../common/constants';
-
-const configValue = 'test';
-const mockConfigService = {
-  getOrThrow: jest.fn().mockReturnValue(configValue),
-};
-
-const mockRedisService = {
-  set: jest.fn(),
-  get: jest.fn(),
-  delete: jest.fn(),
-};
-
-const STABLE_UUID = 'aaaaaaaa-bbbb-4ccc-dddd-eeeeeeeeeeee';
-
-jest.mock('uuid', () => ({
-  v4: () => STABLE_UUID,
-}));
-
-const mockFetch = jest.fn();
-global.fetch = mockFetch;
-
-const mockDatabase = {
-  refreshToken: {
-    create: jest.fn(),
-    findUnique: jest.fn(),
-    delete: jest.fn(),
-  },
-  user: {
-    findUnique: jest.fn(),
-    create: jest.fn(),
-    delete: jest.fn(),
-  },
-};
-
-const mockJwtService = {
-  signAsync: jest.fn(),
-  decode: jest.fn(),
-};
 
 const mockResponse = {
   cookie: jest.fn(),
@@ -74,6 +30,13 @@ const mockLogger = {
   verbose: jest.fn(),
 } as unknown as jest.Mocked<Logger>;
 
+const mockAuthService = {
+  authorize: jest.fn(),
+  callback: jest.fn(),
+  logout: jest.fn(),
+  refresh: jest.fn(),
+};
+
 const mockRequest = {
   cookies: {
     [TOKEN.ACCESS.TYPE]: 'test-access-token',
@@ -86,19 +49,15 @@ describe('AuthController', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [{ useValue: mockAuthService, provide: AuthService }],
       controllers: [AuthController],
-      imports: [DatabaseModule, RedisModule, JwtModule, ConfigModule],
-    })
-      .overrideProvider(ConfigService)
-      .useValue(mockConfigService)
-      .overrideProvider(RedisService)
-      .useValue(mockRedisService)
-      .overrideProvider(DatabaseService)
-      .useValue(mockDatabase)
-      .overrideProvider(JwtService)
-      .useValue(mockJwtService)
-      .compile();
+      imports: [
+        DatabaseModule,
+        RedisModule,
+        JwtModule,
+        ConfigModule.forRoot({ isGlobal: true }),
+      ],
+    }).compile();
 
     controller = module.get<AuthController>(AuthController);
 
@@ -112,101 +71,31 @@ describe('AuthController', () => {
 
   describe('Successful Tests', () => {
     it('should authorize', async () => {
-      mockRedisService.set.mockResolvedValue({ success: true });
+      mockAuthService.authorize.mockResolvedValue('test-url');
       await controller.authorize(mockResponse);
 
       expect(mockResponse.redirect).toHaveBeenCalledWith(
-        expect.stringContaining('https://accounts.google.com/o/oauth2/v2/auth'),
+        expect.stringContaining('test-url'),
       );
     });
 
-    it('should generate the tokens', async () => {
-      mockRedisService.get.mockResolvedValue({
-        success: true,
-        data: { timestamp: Date.now() },
+    it('should handle the google callback', async () => {
+      mockAuthService.callback.mockResolvedValue({
+        accessToken: 'test-access-token',
+        refreshToken: 'test-refresh-token',
       });
-
-      mockFetch.mockResolvedValue({
-        ok: true,
-        json: jest.fn().mockResolvedValue({
-          scope: 'test-scope',
-          id_token: 'test-id-token',
-          access_token: 'test-access-token',
-          refresh_token: 'test-refresh-token',
-        }),
-      });
-
-      mockRedisService.delete.mockResolvedValue({
-        success: true,
-      });
-
-      mockJwtService.decode.mockReturnValue({
-        email: 'test-email@email.com',
-        sub: 'test-sub',
-        name: 'test-name',
-        picture: 'test-picture',
-        iss: 'test-iss',
-        auth_time: 'test-auth-time',
-      });
-
-      mockDatabase.user.findUnique.mockResolvedValue({
-        id: 'test-user-id',
-        email: 'test-email@email.com',
-      });
-
-      mockJwtService.signAsync
-        .mockResolvedValueOnce('test-access-token')
-        .mockResolvedValueOnce('test-refresh-token');
-
-      mockDatabase.refreshToken.create.mockResolvedValue(null);
 
       await controller.callback(mockResponse, 'test-state', 'test-code');
 
       expect(mockResponse.cookie).toHaveBeenCalledTimes(2);
       expect(mockResponse.redirect).toHaveBeenCalledWith(
         302,
-        expect.stringContaining(configValue + '/dashboard'),
+        expect.stringContaining('/dashboard'),
       );
     });
 
     it('should logout the user', async () => {
-      mockJwtService.decode
-        .mockReturnValueOnce({
-          jti: 'test-access-tji',
-        })
-        .mockReturnValueOnce({
-          jti: 'test-refresh-tji',
-        });
-
-      mockRedisService.set.mockResolvedValue({ success: true });
-
-      mockDatabase.refreshToken.findUnique.mockResolvedValue({
-        token_id: 'test-refresh-tji',
-      });
-
-      mockDatabase.refreshToken.delete.mockResolvedValue(null);
-
-      const result = await controller.logout(mockRequest, mockResponse);
-
-      expect(mockResponse.clearCookie).toHaveBeenCalledTimes(2);
-      expect(result).toBeDefined();
-      expect(result).toEqual({
-        message: 'success',
-      });
-    });
-
-    it('should logout the user successfuly if refresh token does not exist in db', async () => {
-      mockJwtService.decode
-        .mockReturnValueOnce({
-          jti: 'test-access-tji',
-        })
-        .mockReturnValueOnce({
-          jti: 'test-refresh-tji',
-        });
-
-      mockRedisService.set.mockResolvedValue({ success: true });
-
-      mockDatabase.refreshToken.findUnique.mockResolvedValue(null);
+      mockAuthService.logout.mockResolvedValue({ message: 'success' });
 
       const result = await controller.logout(mockRequest, mockResponse);
 
@@ -218,25 +107,10 @@ describe('AuthController', () => {
     });
 
     it('should refresh the tokens', async () => {
-      mockJwtService.decode.mockReturnValueOnce({
-        jti: 'test-refresh-tji',
+      mockAuthService.refresh.mockResolvedValue({
+        accessToken: 'test-access-token',
+        refreshToken: 'test-refresh-token',
       });
-
-      mockDatabase.refreshToken.findUnique.mockResolvedValue({
-        user: {
-          id: 'test-user-id',
-          email: 'test-email@email.com',
-        },
-        expires_at: new Date(Date.now() * 10),
-      });
-
-      mockDatabase.refreshToken.delete.mockResolvedValue(null);
-
-      mockJwtService.signAsync
-        .mockResolvedValueOnce('test-access-token')
-        .mockResolvedValueOnce('test-refresh-token');
-
-      mockDatabase.refreshToken.create.mockResolvedValue(null);
 
       const result = await controller.refresh(mockRequest, mockResponse);
 
@@ -246,94 +120,6 @@ describe('AuthController', () => {
       expect(result).toEqual({
         message: 'success',
       });
-    });
-  });
-
-  describe('Unsuccessful Tests', () => {
-    it('should not authorize because of redis failed to store state', async () => {
-      mockRedisService.set.mockResolvedValue({
-        success: false,
-        error: 'Failed state',
-      });
-
-      await expect(controller.authorize(mockResponse)).rejects.toThrow(
-        InternalServerErrorException,
-      );
-    });
-
-    it('should not generate the tokens because of invalid state', async () => {
-      mockRedisService.get.mockResolvedValue({
-        success: true,
-        data: null,
-      });
-
-      await expect(
-        controller.callback(mockResponse, 'test-state', 'test-code'),
-      ).rejects.toThrow(UnauthorizedException);
-    });
-
-    it('should not generate the tokens because fetch to google failed', async () => {
-      mockRedisService.get.mockResolvedValue({
-        success: true,
-        data: { timestamp: Date.now() },
-      });
-
-      mockFetch.mockResolvedValue({
-        ok: false,
-        json: jest.fn().mockResolvedValue({
-          scope: 'test-scope',
-          id_token: 'test-id-token',
-          access_token: 'test-access-token',
-          refresh_token: 'test-refresh-token',
-        }),
-        text: jest.fn().mockResolvedValue('test-error'),
-      });
-
-      await expect(
-        controller.callback(mockResponse, 'test-state', 'test-code'),
-      ).rejects.toThrow(InternalServerErrorException);
-    });
-
-    it('should not refresh the tokens because of invalid refresh token', async () => {
-      mockJwtService.decode.mockReturnValueOnce({
-        jti_invalid: 'test-refresh-tji',
-      });
-
-      await expect(
-        controller.refresh(mockRequest, mockResponse),
-      ).rejects.toThrow(UnauthorizedException);
-    });
-
-    it('should not refresh the tokens because refresh token did not exist in db', async () => {
-      mockJwtService.decode.mockReturnValueOnce({
-        jti: 'test-refresh-tji',
-      });
-
-      mockDatabase.refreshToken.findUnique.mockResolvedValue(null);
-
-      await expect(
-        controller.refresh(mockRequest, mockResponse),
-      ).rejects.toThrow(UnauthorizedException);
-    });
-
-    it('should not refresh the tokens because refresh token has expired', async () => {
-      mockJwtService.decode.mockReturnValueOnce({
-        jti: 'test-refresh-tji',
-      });
-
-      mockDatabase.refreshToken.findUnique.mockResolvedValue({
-        user: {
-          id: 'test-user-id',
-          email: 'test-email@email.com',
-        },
-        expires_at: new Date(1000),
-      });
-
-      mockDatabase.refreshToken.delete.mockResolvedValue(null);
-
-      await expect(
-        controller.refresh(mockRequest, mockResponse),
-      ).rejects.toThrow(UnauthorizedException);
     });
   });
 });
--- src/modules/subscriptions/webhooks/webhooks.controller.spec.ts
+++ src/modules/subscriptions/webhooks/webhooks.controller.spec.ts
@@ -1,7 +1,7 @@
 import { Request } from 'express';
 import { Logger } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 
 import { WebhooksController } from './webhooks.controller';
 
@@ -9,20 +9,6 @@ import { PolarWebhooksService } from './polar-webhooks.service';
 
 import { PolarModule } from '../../../core/polar/polar.module';
 import { DatabaseModule } from '../../../core/database/database.module';
-import { DatabaseService } from '../../../core/database/database.service';
-
-const mockDatabaseService = {
-  subscription: {
-    findUnique: jest.fn(),
-    upsert: jest.fn(),
-    update: jest.fn(),
-  },
-};
-
-const mockConfigService = {
-  getOrThrow: jest.fn().mockReturnValue('test-value'),
-  get: jest.fn().mockReturnValue('test-value'),
-};
 
 const mockLogger = {
   log: jest.fn(),
@@ -33,7 +19,7 @@ const mockLogger = {
   verbose: jest.fn(),
 } as unknown as jest.Mocked<Logger>;
 
-const mockPolarEventService = {
+const mockPolarWebhookEventService = {
   handleEvent: jest.fn(),
 };
 
@@ -54,9 +40,10 @@ describe('WebhooksController', () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [WebhooksController],
       providers: [
-        { provide: PolarWebhooksService, useValue: mockPolarEventService },
-        { useValue: mockConfigService, provide: ConfigService },
-        { useValue: mockDatabaseService, provide: DatabaseService },
+        {
+          provide: PolarWebhooksService,
+          useValue: mockPolarWebhookEventService,
+        },
       ],
       imports: [
         DatabaseModule,
@@ -75,10 +62,11 @@ describe('WebhooksController', () => {
   });
 
   it('should call the polar handle event method', async () => {
+    mockPolarWebhookEventService.handleEvent.mockResolvedValue({});
     await controller.handleEvent(mockRequest);
 
-    expect(mockPolarEventService.handleEvent).toHaveBeenCalled();
-    expect(mockPolarEventService.handleEvent).toHaveBeenCalledWith(
+    expect(mockPolarWebhookEventService.handleEvent).toHaveBeenCalled();
+    expect(mockPolarWebhookEventService.handleEvent).toHaveBeenCalledWith(
       testEventType,
       {},
       testTimestamp,
