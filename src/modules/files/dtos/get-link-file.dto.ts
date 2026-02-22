7d8780c3266dd2c3ad443583334eaf8144c3f1b3
Fashakin Olashubomi <subomifasakin@outlook.com>
2026-02-22 17:51:01 +0100

    updated dto for get link file

--- src/modules/files/dtos/get-link-file.dto.ts
+++ src/modules/files/dtos/get-link-file.dto.ts
@@ -1,5 +1,11 @@
-import { PickType } from '@nestjs/swagger';
+import { ApiProperty, PickType } from '@nestjs/swagger';
 
 import { CreateLinkDto } from './create-link.dto';
+import { IsOptional, IsString } from 'class-validator';
 
-export class GetLinkFileDto extends PickType(CreateLinkDto, ['password']) {}
+export class GetLinkFileDto extends PickType(CreateLinkDto, ['password']) {
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsString({ message: 'Password should be a string' })
+  password?: string | undefined;
+}
