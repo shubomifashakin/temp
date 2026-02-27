57de7074e58c1461cc9d7b4925b0a1aa6a8fba50
Fashakin Olashubomi <subomifasakin@outlook.com>
2026-02-27 15:35:54 +0100

    refactor

--- src/modules/files/files.module.ts
+++ src/modules/files/files.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 
+import { TasksModule } from './tasks/tasks.module';
+import { WebhooksModule } from './webhooks/webhooks.module';
+
 import { FilesService } from './files.service';
 import { FilesController } from './files.controller';
 import { LinksController } from './links/links.controller';
 import { LinksService } from './links/links.service';
-import { WebhooksModule } from './webhooks/webhooks.module';
 
 import { S3Module } from '../../core/s3/s3.module';
 import { SqsModule } from '../../core/sqs/sqs.module';
@@ -12,7 +14,6 @@ import { RedisModule } from '../../core/redis/redis.module';
 import { HasherModule } from '../../core/hasher/hasher.module';
 import { DatabaseModule } from '../../core/database/database.module';
 import { PrometheusModule } from '../../core/prometheus/prometheus.module';
-import { TasksModule } from './tasks/tasks.module';
 
 @Module({
   providers: [FilesService, LinksService],
