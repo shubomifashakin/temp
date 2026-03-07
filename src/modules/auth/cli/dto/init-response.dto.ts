a524b0de86114352120c49298f0192590a2edea4
Fashakin Olashubomi <subomifasakin@outlook.com>
2026-03-07 23:50:08 +0100

    refactor

--- src/modules/auth/cli/dto/init-response.dto.ts
+++ src/modules/auth/cli/dto/init-response.dto.ts
@@ -2,7 +2,9 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class CliAuthInitResponse {
-  @ApiProperty({ description: 'The oauth code' })
+  @ApiProperty({
+    description: 'The authentication code that would be exchanged for a token.',
+  })
   @IsString()
   code: string;
 }
