e68250acdaebc9a1885a1f121612bbd34b0c14d8
Fashakin Olashubomi <subomifasakin@outlook.com>
2026-03-10 22:31:37 +0100

    setup postgres configs for better observability

---
+++ ops/init-exporter.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+  CREATE ROLE postgres_exporter WITH LOGIN PASSWORD '$POSTGRES_EXPORTER_PASSWORD';
+  GRANT pg_monitor TO postgres_exporter;
+  GRANT EXECUTE ON FUNCTION pg_stat_statements_reset() TO postgres_exporter;
+  GRANT SELECT ON pg_stat_statements TO postgres_exporter;
+EOSQL
