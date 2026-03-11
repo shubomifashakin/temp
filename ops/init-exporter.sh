28fbc9f910fb56604f0bd744f16d6d772b411e6f
Fashakin Olashubomi <subomifasakin@outlook.com>
2026-03-11 16:14:14 +0100

    updated conf

--- ops/init-exporter.sh
+++ ops/init-exporter.sh
@@ -5,6 +5,5 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
   CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
   CREATE ROLE postgres_exporter WITH LOGIN PASSWORD '$POSTGRES_EXPORTER_PASSWORD';
   GRANT pg_monitor TO postgres_exporter;
-  GRANT EXECUTE ON FUNCTION pg_stat_statements_reset() TO postgres_exporter;
   GRANT SELECT ON pg_stat_statements TO postgres_exporter;
 EOSQL
